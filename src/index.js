c3ee172b9f9d2aa9151da6ef82826b651805602f
GainorB <gainorbostwick@gmail.com>
2017-10-09 17:04:27 -0400

    route to add a book with dummy data

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const bluebird = require('bluebird');
 
 const auth = require('./routes/auth');
 const users = require('./routes/users');
+const books = require('./routes/books');
 
 dotenv.config();
 const app = express();
@@ -19,6 +20,7 @@ mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
 // ROUTES
 app.use('/api/auth', auth);
 app.use('/api/users', users);
+app.use('/api/books', books);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
